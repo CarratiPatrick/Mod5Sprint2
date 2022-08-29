c842148cf8cbe7959350d4f5fa7966e19d7eb335
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-29 16:28:38 -0300

    Feat Criacao do header com o menu

--- vite-project/src/Button/Button.jsx
+++ vite-project/src/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({texto,click,css}) => {
   return (
-    <div>Button</div>
+    <button onClick={click} className={css}> {texto} </button>
   )
 }
 
---
+++ vite-project/src/Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../Button/Button'
+
+const Menu = () => {
+  return (
+    <div>
+        <nav>
+        <Button texto="Home"/>
+        <Button texto="Cadastrar"/>
+        <Button texto="Clientes"/>
+        </nav>
+    </div>
+  )
+}
+
+export default Menu
